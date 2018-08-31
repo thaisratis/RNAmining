5e00793090ed744e6c17c9026a5a8daa29674eaa
Bruno Correia <brunogomescorreia@gmail.com>
2018-08-31 01:19:43 -0300

    Update pipeline

--- build/php/config/Rscripts/metavolcano_source.R
+++ build/php/config/Rscripts/metavolcano_source.R
@@ -11,7 +11,7 @@ suppressMessages(library(metafor))
 if.geo2rformat <- function(geo2r_res_file, inputfolder) {
 	cnames <- gsub('"', '', unlist(strsplit(system(paste0("head -n 1 ", inputfolder, geo2r_res_file), intern = TRUE), '"\\t"')))
 	if(length(which(cnames %in% c("ID", "adj.P.Val", "P.Value", "t", 
-						   "B", "logFC", "Gene.symbol", "Gene.title", "CI.L", "CI.R"))) != 10) {
+						   "B", "logFC", "Gene.symbol", "Gene.title"))) != 8) {
 		message(paste(geo2r_res_file, "file does not look as a GEO2R output with Confidence Interval..."))
 		FALSE
 	} else {
